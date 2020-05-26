b6682610d0ab4395c0486055cc6b4eb44dc72e1e
Arnaud Duhé <anro010@hotmail.com>
2020-05-26 10:19:12 +0100

    end of quest 5

--- index.js
+++ index.js
@@ -5,31 +5,28 @@ const connection = require('./conf');
 
 
 const bodyParser = require('body-parser');
-// Support JSON-encoded bodies
 app.use(bodyParser.json());
-// Support URL-encoded bodies
 app.use(bodyParser.urlencoded({
 extended: true
 }));
 
-app.post('/api/employees', (req, res) => {
+app.delete('/api/movies/:id', (req, res) => {
 
     // Get the data sent
-    const formData = req.body;
+    const idmovie = req.params.id;
     
-    // connection to the database, and insertion of the employee
-    connection.query('INSERT INTO employee SET ?', formData, (err, results) => {
-    
+    // connection to the database, and insert the movie
+    connection.query('DELETE FROM movie WHERE id = ?', [idmovie], err => {
         if (err) {
         // If an error has occurred, then the user is informed of the error
         console.log(err);
-        res.status(500).send("Error saving an employee");
+        res.status(500).send("Error deleting an movie");
         } else {
         // If everything went well, we send a status "ok".
         res.sendStatus(200);
         }
     });
-});
+    });
 
 app.listen(port, (err) => {
     if (err) {
